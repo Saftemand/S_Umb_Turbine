be0bf66bdd465234d9a8f51aea90df4660b0669f
saftemand <michaelmantonsen@gmail.com>
2017-11-03 09:58:58 +0100

    made a lot of small changes in the frontend

--- S_Umb_Turbine/assets/javascripts/main.js
+++ S_Umb_Turbine/assets/javascripts/main.js
@@ -1,6 +1,8 @@
 ﻿$(document).ready(function () {
 
     $('#booking-create-modal').on('click', function () {
+        $('#create-start-date-row .datetimepicker').data('DateTimePicker').date(moment());
+        $('#create-end-date-row .datetimepicker').data('DateTimePicker').date(moment().add(15,'minutes'));
         $('#create-booking-modal').modal('show');
     })
 
@@ -12,6 +14,7 @@
         $('.list-group-item-room-filter').removeClass('active');
         $(event.target).addClass('active');
         $('#overview-conference-room').val($(event.target).data('conference-room-id'));
+        selectCreateConferenceRoom($(event.target).data('conference-room-id'));
         $(event.target).closest("form").submit();
     });
 
@@ -36,7 +39,11 @@
     $('#logout-button').on('click', function (event) {
         $(event.target).closest("form").submit();
     })
-    
+
+    $('#create-start-date, #create-end-date, #update-start-date, #update-end-date').on('click', function (event) {
+        console.log(event);
+    })
+
     moment.locale('da', {
         week: { dow: 1 } // Monday is the first day of the week
     });
@@ -96,14 +103,16 @@
             });
         },
         eventClick: function (calEvent, jsEvent, view) {
-            
+
             console.log(calEvent);
             if ($.inArray("booked-by-other", calEvent.className)) {
                 $('#update-title').val(calEvent.title);
                 $('#update-conference-room').val(calEvent.conferenceRoomId);
                 $('#update-booked-by').val(calEvent.bookedByName);
                 $('#update-start-date').val(calEvent.start.format('DD-MM-YYYY HH:mm'));
+                $('#update-start-date-row .datetimepicker').data("DateTimePicker").date(calEvent.start.format('DD-MM-YYYY HH:mm'));
                 $('#update-end-date').val(calEvent.end.format('DD-MM-YYYY HH:mm'));
+                $('#update-end-date-row .datetimepicker').data("DateTimePicker").date(calEvent.end.format('DD-MM-YYYY HH:mm'));
                 $('#update-booking-id').val(calEvent.id);
                 $('.list-group-item-room-update').each(function (i, e) {
                     item = $(e)
@@ -145,28 +154,6 @@
         stepping: 15
     });
 
-    $('#update-datetimepicker').datetimepicker();
-    $('#update-datetimepicker').on('dp.change', function (event) {
-        console.log('changing...');
-    });
-
-    var dateTimePickerIds = ['create','update']
-
-    for (var i in dateTimePickerIds) {
-        $('#' + dateTimePickerIds[i] + '-start-datetimepicker').on('dp.change', function (event) {
-            var startTime = event.date;
-            var $endTimeObject = $('#' + dateTimePickerIds[i] + '-end-datetimepicker');
-            var endTimeDateTimePicker = $('#' + dateTimePickerIds[i] + '-end-datetimepicker').data('DateTimePicker');
-            if (endTimeDateTimePicker.date().isBefore(startTime)) {
-                $endTimeObject.data('DateTimePicker').date(moment(startTime.format()).add(15, 'minutes'));
-            }
-        });
-
-        $('#' + dateTimePickerIds[i] + '-start-datetimepicker,' + '#' + dateTimePickerIds[i] + '-end-datetimepicker').on('dp.show', function (event) {
-            $("a[data-action='togglePicker']").trigger('click');
-        })
-    }
-    
     $('#overview-conference-room').on('change', function () {
         $("#overview-calendar-form").submit();
     });
@@ -174,7 +161,7 @@
     $('#overview-show-only-current-bookings').on('change', function () {
         $("#overview-calendar-form").submit();
     });
-    
+
 
     $('#overview-calendar-form').on('submit', function (event) {
         event.preventDefault();
@@ -203,6 +190,9 @@
         event.stopPropagation();
 
         var postData = $(this).serializeArray();
+        if (!validateCreateForm(postData, 'create')) {
+            return;
+        }
         var formURL = $(this).attr("action");
 
         $.ajax({
@@ -230,7 +220,7 @@
                 }
             },
             error: function (jqXHR, textStatus, errorThrown) {
-                
+
             }
         });
     });
@@ -240,6 +230,11 @@
         event.stopPropagation();
 
         var postData = $(this).serializeArray();
+
+        if (!validateCreateForm(postData, 'update')) {
+            return;
+        }
+
         var formURL = $(this).attr("action");
         $.ajax({
             url: "/api/BookingHandler.ashx?action=validateTime",
@@ -247,7 +242,7 @@
             data: postData,
             success: function (data, textStatus, jqXHR) {
                 var parsedData = JSON.parse(data);
-                if (parsedData.message == 'OPEN'){
+                if (parsedData.message == 'OPEN') {
                     $.ajax({
                         url: formURL,
                         type: "POST",
@@ -268,10 +263,10 @@
                 }
             },
             error: function (jqXHR, textStatus, errorThrown) {
-                
+
             }
         });
-        
+
     });
 
     $('#delete-booking-form').on('submit', function (event) {
@@ -291,7 +286,7 @@
                 $('#delete-view-booking-modal').modal('hide');
             },
             error: function (jqXHR, textStatus, errorThrown) {
-                
+
             }
         });
     });
@@ -299,7 +294,7 @@
     $('#change-password-form').on('submit', function (event) {
         event.preventDefault();
         event.stopPropagation();
-        
+
         var postData = $(this).serializeArray();
         var formURL = $(this).attr("action");
         $.ajax({
@@ -310,10 +305,83 @@
                 console.log(data);
             },
             error: function (jqXHR, textStatus, errorThrown) {
-                
+
             }
         });
 
     })
 
+    var validateCreateForm = function (serializedForm, context) {
+        var validated = true;
+        var formObject = {}
+
+        for (var i in serializedForm) {
+            formObject[serializedForm[i].name] = serializedForm[i].value;
+        }
+
+        if (formObject[context + '-conference-room'] == '') {
+            validated = false;
+            $('#' + context + '-conference-room-row').addClass('has-danger');
+            $('#' + context + '-conference-room-feedback').show();
+        } else {
+            $('#' + context + '-conference-room-row').removeClass('has-danger');
+            $('#' + context + '-conference-room-feedback').hide();
+        }
+
+        var startDate = formObject[context + '-start-date'];
+        var endDate = formObject[context + '-end-date'];
+
+        //check if startDate comes before endDate
+        if (!moment(startDate, 'DD-MM-YYYY HH:mm').isBefore(moment(endDate, 'DD-MM-YYYY HH:mm'))) {
+            validated = false;
+            $('#' + context + '-end-date-row').addClass('has-danger');
+            $('#' + context + '-end-date-feedback').show();
+        } else {
+            $('#' + context + '-end-date-row').removeClass('has-danger');
+            $('#' + context + '-end-date-feedback').hide();
+        }
+
+
+        return validated;
+    };
+
+    var dateTimePickerIds = ['create', 'update']
+
+    dateTimePickerIds.forEach(function (item, index) {
+        var startDateTimePicker = $('#' + item + '-start-date-row .datetimepicker');
+        startDateTimePicker.on('dp.change', function (event) {
+            var startTime = event.date;
+            var $endTimeObject = $('#' + item + '-end-date-row .datetimepicker');
+            var endTimeDateTimePicker = $endTimeObject.data('DateTimePicker');
+            if (endTimeDateTimePicker.date().isBefore(startTime)) {
+                $endTimeObject.data('DateTimePicker').date(moment(startTime.format()).add(15, 'minutes'));
+            }
+        });
+
+        var endDateTimePicker = $('#' + item + '-end-date-row .datetimepicker');
+        endDateTimePicker.on('dp.change', function (event) {
+            var endTime = event.date;
+            var $startTimeObject = $('#' + item + '-start-date-row .datetimepicker');
+            var startTimeDateTimePicker = $startTimeObject.data('DateTimePicker');
+            if (startTimeDateTimePicker.date().isAfter(endTime)) {
+                $startTimeObject.data('DateTimePicker').date(moment(endTime.format()).subtract(15, 'minutes'));
+            }
+        });
+
+        $('#' + item + '-start-date-row .datetimepicker, ' + '#' + item + '-end-date-row .datetimepicker').on('dp.show', function (event) {
+            $("a[data-action='togglePicker']").trigger('click');
+        })
+    })
+
+    var selectCreateConferenceRoom = function (roomId) {
+        var rooms = $('.list-group-item-room-create');
+        rooms.removeClass('active');
+        var room = rooms.filter("[data-conference-room-id=" + roomId + "]");
+        room.addClass('active');
+        
+        $('#create-conference-room').val(roomId);
+    }
+
+    selectCreateConferenceRoom($($('.list-group-item-room-create')[0]).data('conference-room-id'));
+    
 });
